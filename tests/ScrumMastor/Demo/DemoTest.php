303f4aebb6cee6b6e394b6cde65f1dfa962710b2
Jalouzot <jjalouzot@eleven-labs.com>
2014-01-28 22:46:35 +0100

    Add Test

---
+++ tests/ScrumMastor/Demo/DemoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tests\ScrumMastor\Demo;
+
+
+class DemoTest extends \PHPUnit_Framework_TestCase
+{
+	public function testDemo()
+    {
+        $this->assertEquals(true, true);
+    }
+}
