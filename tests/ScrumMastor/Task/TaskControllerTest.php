e4df1ba39521aad669c51eeccf6aea22e61981c9
Jalouzot <jjalouzot@eleven-labs.com>
2014-02-16 21:55:05 +0100

    All tests

---
+++ tests/ScrumMastor/Task/TaskControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tests\ScrumMastor\Task;
+
+
+class TaskTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSave()
+    {
+        $this->assertEquals(true, true);
+    }
+}
