bf0e296cfe6e9cecba0ca101ae06e5a69a16e744
Pouzor <remy.jardinet@gmail.com>
2014-02-21 10:49:28 +0000

    test

--- config/default.php
+++ config/default.php
@@ -4,7 +4,7 @@
 
 return array(
 	'debug' => true,
-        'mongo.server' => 'mongodb://jonathan:test@troup.mongohq.com:10022',
+        'mongo.server' => 'ddmongodb://jonathan:test@troup.mongohq.com:10022',
 	'mongo.options' => array(
 		'db' => 'scrummastor'
 		),
