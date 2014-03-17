eade17705b9d969796ee081865d196ceb33a7d4e
Thierry Piaf <thierry@piaf.eu>
2014-03-17 17:42:30 +0100

    Fix CS

--- tests/ScrumMastor/Task/TaskControllerTest.php
+++ tests/ScrumMastor/Task/TaskControllerTest.php
@@ -2,10 +2,10 @@
 
 namespace tests\ScrumMastor\Task;
 
-
-class TaskTest extends \Silex\WebTestCase
+class TaskControllerTest extends \Silex\WebTestCase
 {
-    public function createApplication() {
+    public function createApplication()
+    {
         return require __DIR__ . '/../../../web/index_test.php';
     }
 
@@ -13,40 +13,45 @@ class TaskTest extends \Silex\WebTestCase
     {
         $this->assertEquals(true, true);
     }
-    
-    public function testDeleteEmptyParameter(){
+
+    public function testDeleteEmptyParameter()
+    {
         $client = $this->createClient();
         $client->request("DELETE", "/task");
         $this->assertEquals(500, $client->getResponse()->getStatusCode());
         $this->assertEquals("\"ID Parameter is empty\"", $client->getResponse()->getContent());
     }
-    
-    public function testDeleteInvalidParameter(){
+
+    public function testDeleteInvalidParameter()
+    {
         $client = $this->createClient();
         $client->request("DELETE", "/task", array('id'=>'random'));
         $this->assertEquals(500, $client->getResponse()->getStatusCode());
         $this->assertEquals("\"ID Parameter is invalid\"", $client->getResponse()->getContent());
     }
-    
-    public function testDeleteUnknowId(){
+
+    public function testDeleteUnknowId()
+    {
         $client = $this->createClient();
         $client->request("DELETE", "/task", array('id'=>'530f79b2d58c90be0a0041a7'));
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
         $this->assertEquals("\"Task not found\"", $client->getResponse()->getContent());
     }
-    
-    public function testDeleteFixture(){
+
+    public function testDeleteFixture()
+    {
         $client = $this->createClient();
         $client->request('POST', '/task', array('title' => 'Test unit', 'description' => 'Task use in test unit'));
         $this->assertEquals($client->getResponse()->getStatusCode(), 200);
         $this->assertEquals($client->getResponse()->getContent(), "\"true\"");
     }
-    
-    public function testDelete(){
+
+    public function testDelete()
+    {
         $client = $this->createClient();
         $tasks = $this->app['mongo']->tasks->find(array('title'=>'Test unit'));
         $tasks = iterator_to_array($tasks);
-        foreach($tasks as $key=>$value){
+        foreach ($tasks as $key=>$value) {
             $client->request("DELETE", "/task", array('id'=>  $key));
             $this->assertEquals(200, $client->getResponse()->getStatusCode());
             $this->assertEquals("\"true\"", $client->getResponse()->getContent());
