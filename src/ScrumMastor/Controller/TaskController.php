16a8beebbc0e4186e480eff0934b28a979232b9e
Thierry Piaf <thierry@piaf.eu>
2014-03-17 17:42:30 +0100

    Fix CS

--- src/ScrumMastor/Controller/TaskController.php
+++ src/ScrumMastor/Controller/TaskController.php
@@ -4,59 +4,58 @@ namespace ScrumMastor\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class TaskController
 {
-	protected $mongo;
-	protected $request;
-	
-	public function __construct($mongo, Request $request)
-	{
-		$this->mongo = $mongo;
-		$this->request = $request;
-	}
+    protected $mongo;
+    protected $request;
 
-	public function saveAction()
-	{
-		$title = $this->request->get('title');
-		if (!isset($title)){
-			return new JsonResponse('false', 500);
-		}
-		
-		$this->mongo->tasks->insert(array('title' =>  $title, 'description' =>  $this->request->get('description', '')));
+    public function __construct($mongo, Request $request)
+    {
+        $this->mongo = $mongo;
+        $this->request = $request;
+    }
 
-		return new JsonResponse('true', 200);
-	}
-    
+    public function saveAction()
+    {
+        $title = $this->request->get('title');
+        if (!isset($title)) {
+            return new JsonResponse('false', 500);
+        }
+
+        $this->mongo->tasks->insert(array('title' =>  $title, 'description' =>  $this->request->get('description', '')));
+
+        return new JsonResponse('true', 200);
+    }
+
     /**
      * Delete task by ID
      * @return JsonResponse String and HTTP Code
      */
-    public function deleteAction()
+    public function deleteAction($id)
     {
         $id = $this->request->get('id');
         
-        if(empty($id)){
+        if (empty($id)) {
             return new JsonResponse("ID Parameter is empty", 500); //input invalid
         }
-        
+
         try {
             $mongoId = new \MongoId($id);
         } catch (\MongoException $e) {
             return new JsonResponse("ID Parameter is invalid", 500); //input invalid
         }
-        
+
         $task = $this->mongo->tasks->find(array('_id' => new \MongoId($id)), array('_id'=>true));
 
-        if(!count(iterator_to_array($task))){
+        if (!count(iterator_to_array($task))) {
             return new JsonResponse("Task not found", 404); //id not found
         }
-        
+
         $return = $this->mongo->tasks->remove(array('_id' => new \MongoId($id)));
-        if(is_null($return['err'])){
+        if (is_null($return['err'])) {
             return new JsonResponse('true', 200); //delete with success
-        }else{
+        } else {
             return new JsonResponse('false', 500); //cannot remove
         }
     }
