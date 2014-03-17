0ca350b58f62c0a1c1797d3665cb3f402b74fcbb
Thierry Piaf <thierry@piaf.eu>
2014-03-17 17:12:29 +0100

    Implement Delete Task

--- src/ScrumMastor/Controller/TaskController.php
+++ src/ScrumMastor/Controller/TaskController.php
@@ -28,5 +28,37 @@ class TaskController
 
 		return new JsonResponse('true', 200);
 	}
+    
+    /**
+     * Delete task by ID
+     * @return JsonResponse String and HTTP Code
+     */
+    public function deleteAction()
+    {
+        $id = $this->request->get('id');
+        
+        if(empty($id)){
+            return new JsonResponse("ID Parameter is empty", 500); //input invalid
+        }
+        
+        try {
+            $mongoId = new \MongoId($id);
+        } catch (\MongoException $e) {
+            return new JsonResponse("ID Parameter is invalid", 500); //input invalid
+        }
+        
+        $task = $this->mongo->tasks->find(array('_id' => new \MongoId($id)), array('_id'=>true));
+
+        if(!count(iterator_to_array($task))){
+            return new JsonResponse("Task not found", 404); //id not found
+        }
+        
+        $return = $this->mongo->tasks->remove(array('_id' => new \MongoId($id)));
+        if(is_null($return['err'])){
+            return new JsonResponse('true', 200); //delete with success
+        }else{
+            return new JsonResponse('false', 500); //cannot remove
+        }
+    }
 
 }
