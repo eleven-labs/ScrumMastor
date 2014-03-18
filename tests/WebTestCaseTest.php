1211ec43d3cf209b1783778194748cf381d39632
Thierry Piaf <thierry@piaf.eu>
2014-03-18 12:34:05 +0100

    Refactoring WebTestCase

--- tests/WebTestCaseTest.php
+++ tests/WebTestCaseTest.php
@@ -1,13 +1,14 @@
 <?php
 namespace tests;
+
 use Silex\WebTestCase as BaseWebTestCase;
 use ScrumMastor\ScrumMastorApplication;
+
 class WebTestCaseTest extends BaseWebTestCase
 {
     public function createApplication()
     {
         $app = new ScrumMastorApplication(array('env' => 'test'));
-
         return $app;
     }
 
