e1d2327af5c371713ec9c5ac1398c1cddee5ecb2
Thierry Piaf <thierry@piaf.eu>
2014-03-18 11:45:33 +0100

    Move init test app into tests

--- tests/ScrumMastor/Task/TaskControllerTest.php
+++ tests/ScrumMastor/Task/TaskControllerTest.php
@@ -6,7 +6,7 @@ class TaskControllerTest extends \Silex\WebTestCase
 {
     public function createApplication()
     {
-        return require __DIR__ . '/../../../web/index_test.php';
+        return require __DIR__ . '/../../bootstrap.php';
     }
 
     public function testSave()
@@ -24,7 +24,7 @@ class TaskControllerTest extends \Silex\WebTestCase
     public function testDeleteInvalidParameter()
     {
         $client = $this->createClient();
-        $client->request("DELETE", "/task/random", array('id'=>'random'));
+        $client->request("DELETE", "/task/random", array('id' => 'random'));
         $this->assertEquals(500, $client->getResponse()->getStatusCode());
         $this->assertEquals("\"ID Parameter is invalid\"", $client->getResponse()->getContent());
     }
@@ -32,7 +32,7 @@ class TaskControllerTest extends \Silex\WebTestCase
     public function testDeleteUnknowId()
     {
         $client = $this->createClient();
-        $client->request("DELETE", "/task/530f79b2d58c90be0a0041a7", array('id'=>'530f79b2d58c90be0a0041a7'));
+        $client->request("DELETE", "/task/530f79b2d58c90be0a0041a7", array('id' => '530f79b2d58c90be0a0041a7'));
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
         $this->assertEquals("\"Task not found\"", $client->getResponse()->getContent());
     }
@@ -51,7 +51,7 @@ class TaskControllerTest extends \Silex\WebTestCase
         $tasks = $this->app['mongo']->tasks->find(array('title'=>'Test unit'));
         $tasks = iterator_to_array($tasks);
         foreach ($tasks as $key=>$value) {
-            $client->request("DELETE", "/task/".$key, array('id'=>  $key));
+            $client->request("DELETE", "/task/".$key, array('id' => $key));
             $this->assertEquals(200, $client->getResponse()->getStatusCode());
             $this->assertEquals("\"true\"", $client->getResponse()->getContent());
         }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 use ScrumMastor\ScrumMastorApplication;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new ScrumMastorApplication(array('env' => 'test'));
 return $app;
+
+
