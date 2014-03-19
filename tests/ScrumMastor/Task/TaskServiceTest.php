af974e8b9d79864b428834ac94266b659755ef8b
Thierry Piaf <thierry@piaf.eu>
2014-03-19 16:24:02 +0100

    Add test unit

--- tests/ScrumMastor/Task/TaskServiceTest.php
+++ tests/ScrumMastor/Task/TaskServiceTest.php
@@ -50,4 +50,53 @@ class TaskServiceTest extends WebTestCaseTest
         $return = $taskService->existId("azer");
         $this->assertFalse($return);
     }
+    
+    public function testInsertTask(){
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->insertTask(array('title' => 'Test unit insert', 'description' => 'Test unit insert description'));
+        $this->assertTrue($return);
+    }
+    
+    public function testInsertTaskEmpty(){
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->insertTask(array());
+        $this->assertFalse($return);
+    }
+    
+    public function testInsertTaskString(){
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->insertTask('stagea');
+        $this->assertFalse($return);
+    }
+    
+    public function testRemoveTask(){
+        $data = array('title' => 'Test service as unit', 'description' => 'I can haz cheezburger');
+        $this->app['mongo']->tasks->insert($data);
+        
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->removeTask(array('title' => 'Test service as unit'));
+        $this->assertTrue($return);
+    }
+    
+    public function testRemoveTaskAll(){
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->removeTask(array());
+        $this->assertTrue($return);
+    }
+    
+    public function testRemoveTaskString(){
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->removeTask('stagea');
+        $this->assertFalse($return);
+    }
+    
+    public function testUpdateTask(){
+        $data = array('title' => 'Test service as unit', 'description' => 'I can haz cheezburger');
+        $this->app['mongo']->tasks->insert($data);
+        
+        $taskService = new \ScrumMastor\Service\TaskService($this->app['mongo']);
+        $return = $taskService->updateTask(array('_id' => $data['_id']),
+                array('$set' => array('title' => 'egaegaeg')));
+        $this->assertTrue($return);
+    }
 }
